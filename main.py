618ce52f84d995b6ac4bd8247d4c1d466d3f1533
JH Gu <rnwjdgus96@gmail.com>
2022-12-05 20:13:03 +0900

    added real-time token price data code

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import networkx as nx
+import plotly.graph_objects as go
 from datetime import datetime
+import requests
+
+token_dict = {"ETH": "ethereum"}
 
 st.title('On-chain Data Visualizer')
 
 st.header('Dashboard')
 col1, col2, col3 = st.columns(3)
 with col1:
-    # st.subheader('Fear and Greed Index')
     st.image('https://alternative.me/crypto/fear-and-greed-index.png', caption='Fear and Greed Index', use_column_width=True)
-# <img src="https://alternative.me/crypto/fear-and-greed-index.png" alt="Latest Crypto Fear & Greed Index" />
 st.header('Choose Token & Time range')
 col1, col2 = st.columns(2)
 with col1:
@@ -37,20 +40,31 @@ with col2:
             format="YYYY-MM-DD")
         st.write('Start date:', start)
         st.write('End date:', end)
-st.header('Price Chart')
-col1, col2, col3, col4 = st.columns(4)
-col1.metric("Current price", "$1,281.66", "10%")
-col2.metric("Market Cap", "$156,930,933,370", "9%")
-col3.metric("Volume (24h)", "$8,189,875,958", "13%")
-col4.metric("Last 7 Days", "picture")
-st.image('https://images.theconversation.com/files/380042/original/file-20210121-23-1bblai1.png?ixlib=rb-1.1.0&q=30&auto=format&w=600&h=322&fit=crop&dpr=2', caption=f"{token} price chart", use_column_width=True)
+if token == "Select token":
+    st.header('Token Price Chart')
+else:
+    st.header(f'{token} Price Chart')
+    col1, col2, col3, col4 = st.columns(4)
+    coin_url = f"https://api.coingecko.com/api/v3/coins/{token_dict[token]}/market_chart?vs_currency=usd&days=0&interval=daily"
+    coin_data = requests.get(coin_url).json()
+
+    price = coin_data['prices'][0][1]
+    market_cap = coin_data['market_caps'][0][1] / 1000000
+    volume = coin_data['total_volumes'][0][1] / 1000000
+
+    col1.metric("Current price", f"${price:,.2f}")
+    col2.metric("Market Cap", f"${market_cap:,.1f}M")
+    col3.metric("Volume (24h)", f"${volume:,.1f}M")
+    with col4:
+        st.image("https://www.coingecko.com/coins/279/sparkline", caption='Last 7 days Price Chart', use_column_width=True)
 
 st.header('On-chain Network Visualization')
 col1, col2 = st.columns([3, 1])
-with col1:
-    st.image('https://supplychainbeyond.com/wp-content/uploads/2019/08/supply-chain-network-applications-multi-party-networks-5.jpg', caption='On-chain Network', use_column_width=True)
 with col2:
     user_min = st.slider('Adjust MIN value',
                 min_value=0,
                 max_value=100,
                 value=40)
+with col1:
+    st.image('https://supplychainbeyond.com/wp-content/uploads/2019/08/supply-chain-network-applications-multi-party-networks-5.jpg', caption='On-chain Network', use_column_width=True)
+    # st.plotly_chart()
