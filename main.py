5701d57751b8424c88d38878fe80b4efd8b579f6
JH Gu <rnwjdgus96@gmail.com>
2022-12-06 17:40:44 +0900

    modified dashboard

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 from datetime import datetime
 import requests
 
-token_dict = {"ETH": "ethereum"}
+token_dict = {"BTC": "bitcoin", "ETH": "ethereum"}
 
 st.title('On-chain Data Visualizer')
 
@@ -14,6 +14,20 @@ st.header('Dashboard')
 col1, col2, col3 = st.columns(3)
 with col1:
     st.image('https://alternative.me/crypto/fear-and-greed-index.png', caption='Fear and Greed Index', use_column_width=True)
+
+with col2:
+    btc_url = f"https://api.coingecko.com/api/v3/coins/{token_dict['BTC']}/market_chart?vs_currency=usd&days=0&interval=daily"
+    btc_data = requests.get(btc_url).json()
+    btc_price = btc_data['prices'][0][1]
+    st.metric("BTC Price", f"${btc_price:,.2f}")
+    st.image('https://www.coingecko.com/coins/1/sparkline', caption='Last 7 days BTC Price Chart', use_column_width=True)
+with col3:
+    trending_url = "https://api.coingecko.com/api/v3/search/trending"
+    trending_data = requests.get(trending_url).json()
+    trending_coins = [x['item']['name'] for x in trending_data['coins']]
+    st.write("Trending Coins")
+    st.write(trending_coins)
+
 st.header('Choose Token & Time range')
 col1, col2 = st.columns(2)
 with col1:
@@ -41,9 +55,15 @@ with col2:
         st.write('Start date:', start)
         st.write('End date:', end)
 if token == "Select token":
-    st.header('Token Price Chart')
+    st.header('Token Price Info')
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("Current price", f"${0.00}")
+    col2.metric("Market Cap", f"${0.0}M")
+    col3.metric("Volume (24h)", f"${0.0}M")
+    with col4:
+        st.image('https://www.coingecko.com/coins/1/sparkline', caption='Last 7 days Price Chart', use_column_width=True)
 else:
-    st.header(f'{token} Price Chart')
+    st.header(f'{token} Price Info')
     col1, col2, col3, col4 = st.columns(4)
     coin_url = f"https://api.coingecko.com/api/v3/coins/{token_dict[token]}/market_chart?vs_currency=usd&days=0&interval=daily"
     coin_data = requests.get(coin_url).json()
@@ -59,12 +79,12 @@ else:
         st.image("https://www.coingecko.com/coins/279/sparkline", caption='Last 7 days Price Chart', use_column_width=True)
 
 st.header('On-chain Network Visualization')
-col1, col2 = st.columns([3, 1])
-with col2:
+col1, _ = st.columns(2)
+with col1:
     user_min = st.slider('Adjust MIN value',
                 min_value=0,
                 max_value=100,
                 value=40)
-with col1:
-    st.image('https://supplychainbeyond.com/wp-content/uploads/2019/08/supply-chain-network-applications-multi-party-networks-5.jpg', caption='On-chain Network', use_column_width=True)
-    # st.plotly_chart()
+st.image('https://supplychainbeyond.com/wp-content/uploads/2019/08/supply-chain-network-applications-multi-party-networks-5.jpg', caption='On-chain Network', use_column_width=True)
+# st.plotly_chart()
+st.write(user_min)
