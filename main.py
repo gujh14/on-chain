321f9d464023fceb56746dee67bc558afd7a3009
JH Gu <rnwjdgus96@gmail.com>
2022-12-07 20:28:40 +0900

    added price chart

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import pandas as pd
 import networkx as nx
 from pyvis.network import Network
 
+import plotly.graph_objects as go
 import datetime as dt
 import requests
 import time
 
 from data import get_data
 from data import getDateRange
+
+import san
+san.ApiConfig.api_key = "5eltgwflu5h23c3v_f2ky7f3nxsufx2m4"
 
 ticker2name = {
     "BTC": "bitcoin",
@@ -50,6 +54,13 @@ def call_coin_data(token):
     coin_data = requests.get(coin_url).json()
     return coin_data
 
+@st.cache
+def call_ohlc_data(token, from_date, to_date, interval):
+    ohlc_data = san.get(
+        "ohlc", slug=f"{ticker2name[token]}", from_date=from_date, to_date=to_date, interval=interval
+    )
+    return ohlc_data
+
 st.header('Dashboard')
 col1, col2, col3 = st.columns(3)
 # Fear and Greed Index
@@ -70,36 +81,40 @@ with col3:
 
 st.header('Choose Token')
 token = st.selectbox('Select token ticker',
-                options=('Select token','ETH','UNI','LINK','APE'),
+                options=('ETH','UNI','LINK','APE'),
                 index=0)
 
-st.header(f'Real-time {token} Token Metrics' if token != "Select token" else "Real-time Token Metrics")
+st.header(f'Real-time {token} Token Metrics')
 col1, col2, col3, col4 = st.columns(4)
-if token != "Select token":
-    coin_data = call_coin_data(token)
-    
-token_price = coin_data['prices'][0][1] if token != "Select token" else 0.00
-token_market_cap = coin_data['market_caps'][0][1] / 1000000 if token != "Select token" else 0.0
-token_volume = coin_data['total_volumes'][0][1] / 1000000 if token != "Select token" else 0.0
+coin_data = call_coin_data(token)
+
+token_price = coin_data['prices'][0][1] ## if token != "Select token" else 0.00
+token_market_cap = coin_data['market_caps'][0][1] / 1000000 ## if token != "Select token" else 0.0
+token_volume = coin_data['total_volumes'][0][1] / 1000000 ## if token != "Select token" else 0.0
 col1.metric("Current price", f"${token_price:,.2f}")
 col2.metric("Market Cap", f"${token_market_cap:,.1f}M")
 col3.metric("Volume (24h)", f"${token_volume:,.1f}M")
 with col4:
-    img_url = ticker2imgurl[token] if token != 'Select token' else ticker2imgurl['BTC'] # this image is for ETH only. Need to change here for each token
+    img_url = ticker2imgurl[token]
     st.image(img_url, caption='Last 7 days Price Chart', use_column_width=True)
+
+min_date, max_date = getDateRange(token)
+
+ohlc_data = call_ohlc_data(token, str(min_date), str(max_date), "1d")
+fig = go.Figure(data=[go.Candlestick(x=ohlc_data.index, open=ohlc_data['openPriceUsd'], high=ohlc_data['highPriceUsd'], low=ohlc_data['lowPriceUsd'], close=ohlc_data['closePriceUsd'])])
+fig.update_layout(xaxis_rangeslider_visible=False, title=f'{token} Price Chart from {min_date} to {max_date}')
+st.plotly_chart(fig, use_container_width=True)
 
 st.header('Choose Datetime range')
 with st.form("Choose Date range"):
     # Select date range
     st.write('Select date range (Time Zone: UTC)')
-    min_date, max_date = getDateRange(token)
+    
     user_start_date = st.date_input(f'Start date: from {min_date}', min_date, disabled=False)
     user_end_date = st.date_input(f'End date: to {max_date}', min_date + dt.timedelta(days=2), disabled=False)
     date_submitted = st.form_submit_button("Continue")
     if date_submitted:
-        if token == 'Select token':
-            st.warning('Please select token.')
-        elif user_end_date < user_start_date:
+        if user_end_date < user_start_date:
             st.warning('End date must fall after start date.')
         elif user_start_date < min_date:
             st.warning(f'Start date must be after {min_date}.')
@@ -126,9 +141,7 @@ with st.form("Choose Time range"):
 
 st.header('On-chain Network Visualization')
 
-# st.text(f"Token: {token}" if token != "Select token" else "Token: ")
-# st.text(f"From: {user_start_time}")
-# st.text(f"To: {user_end_time}")
+st.text(f"On-chain Network of {token} from {user_start_time} to {user_end_time} (UTC)")
 
 min_value = 0
 max_value = 0
@@ -143,45 +156,45 @@ user_min = st.slider('Adjust MIN value',min_value=min_value,max_value=max_value,
 
 st.write(user_min)
 
-if token != "Select token":
-    df = get_data(token, str(user_start_time), str(user_end_time), user_min, 50)
+# if token != "Select token":
+df = get_data(token, str(user_start_time), str(user_end_time), user_min, 50)
+
+print(str(user_start_time))
+print(str(user_end_time))
+print(user_min)
+# Create networkx graph object from pandas dataframe
+G = nx.from_pandas_edgelist(df, source='From', target='To', edge_attr='Value', create_using=nx.MultiGraph())
+d=dict(G.degree)
+scale=10
+#Updating edge weighted degree dict
+d.update((x, scale*y) for x, y in d.items())
     
-    print(str(user_start_time))
-    print(str(user_end_time))
-    print(user_min)
-    # Create networkx graph object from pandas dataframe
-    G = nx.from_pandas_edgelist(df, source='From', target='To', edge_attr='Value', create_using=nx.MultiGraph())
-    d=dict(G.degree)
-    scale=10
-    #Updating edge weighted degree dict
-    d.update((x, scale*y) for x, y in d.items())
-        
-    #Setting up size attribute
-    nx.set_node_attributes(G,d,'size')
-        
-    # Initiate PyVis network object
-    coin_net = Network(height='465px', bgcolor='#222222', font_color='white')
+#Setting up size attribute
+nx.set_node_attributes(G,d,'size')
+    
+# Initiate PyVis network object
+coin_net = Network(height='465px', bgcolor='#222222', font_color='white')
 
-    # Take Networkx graph and translate it to a PyVis graph format
-    coin_net.from_nx(G)
+# Take Networkx graph and translate it to a PyVis graph format
+coin_net.from_nx(G)
 
-    # Generate network with specific layout settings
-    coin_net.repulsion(node_distance=420, central_gravity=0.33,
-                        spring_length=110, spring_strength=0.10,
-                        damping=0.95)
+# Generate network with specific layout settings
+coin_net.repulsion(node_distance=420, central_gravity=0.33,
+                    spring_length=110, spring_strength=0.10,
+                    damping=0.95)
 
-    # Save and read graph as HTML file (on Streamlit Sharing)
-    try:
-        path = '/tmp'
-        coin_net.save_graph(f'{path}/pyvis_graph.html')
-        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+# Save and read graph as HTML file (on Streamlit Sharing)
+try:
+    path = '/tmp'
+    coin_net.save_graph(f'{path}/pyvis_graph.html')
+    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
-    # Save and read graph as HTML file (locally)
-    except:
-        path = '/html_files'
-        coin_net.save_graph(f'{path}/pyvis_graph.html')
-        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+# Save and read graph as HTML file (locally)
+except:
+    path = '/html_files'
+    coin_net.save_graph(f'{path}/pyvis_graph.html')
+    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
-    # Load HTML file in HTML component for display on Streamlit page
-    components.html(HtmlFile.read(), height=435)
+# Load HTML file in HTML component for display on Streamlit page
+components.html(HtmlFile.read(), height=435)
 
