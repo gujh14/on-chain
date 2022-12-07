e22ebbd2bd6887faf066da106f6208d92d127f0d
JH Gu <rnwjdgus96@gmail.com>
2022-12-07 17:15:32 +0900

    added caching code for api

--- main.py
+++ main.py
@@ -13,9 +13,42 @@ import time
 
 from data import get_data
 
-token_dict = {"BTC": "bitcoin", "ETH": "ethereum", "UNI": "uniswap", "LINK": "chainlink", "APE": "apecoin"}
+ticker2name = {
+    "BTC": "bitcoin",
+    "ETH": "ethereum",
+    "UNI": "uniswap",
+    "LINK": "chainlink",
+    "APE": "apecoin"
+    }
+ticker2imgurl = {
+    "BTC": "https://www.coingecko.com/coins/1/sparkline", 
+    "ETH": "https://www.coingecko.com/coins/279/sparkline", 
+    "UNI": "https://www.coingecko.com/coins/12504/sparkline", 
+    "LINK": "https://www.coingecko.com/coins/877/sparkline", 
+    "APE": "https://www.coingecko.com/coins/14496/sparkline"
+    }
 
 st.title('On-chain Data Visualizer')
+
+@st.cache
+def call_btc_data():
+    # api call
+    btc_url = f"https://api.coingecko.com/api/v3/coins/{ticker2name['BTC']}/market_chart?vs_currency=usd&days=0&interval=daily"
+    btc_data = requests.get(btc_url).json()
+    return btc_data
+
+@st.cache
+def call_trending_data():
+    # api call
+    trending_url = "https://api.coingecko.com/api/v3/search/trending"
+    trending_data = requests.get(trending_url).json()
+    return trending_data
+
+@st.cache
+def call_coin_data(token):
+    coin_url = f"https://api.coingecko.com/api/v3/coins/{ticker2name[token]}/market_chart?vs_currency=usd&days=0&interval=daily"
+    coin_data = requests.get(coin_url).json()
+    return coin_data
 
 st.header('Dashboard')
 col1, col2, col3 = st.columns(3)
@@ -24,28 +57,39 @@ with col1:
     st.image('https://alternative.me/crypto/fear-and-greed-index.png', caption='Fear and Greed Index', use_column_width=True)
 # BTC Price
 with col2:
-    btc_url = f"https://api.coingecko.com/api/v3/coins/{token_dict['BTC']}/market_chart?vs_currency=usd&days=0&interval=daily"
-    btc_data = requests.get(btc_url).json()
+    btc_data = call_btc_data()
     btc_price = btc_data['prices'][0][1]
     st.metric("BTC Price", f"${btc_price:,.2f}")
     st.image('https://www.coingecko.com/coins/1/sparkline', caption='Last 7 days BTC Price Chart', use_column_width=True)
 # Trending Coins
 with col3:
-    trending_url = "https://api.coingecko.com/api/v3/search/trending"
-    trending_data = requests.get(trending_url).json()
+    trending_data = call_trending_data()
     trending_coins = [x['item']['name'] for x in trending_data['coins']]
     st.write("Trending Coins")
     st.write(trending_coins)
 
-st.header('Choose Token & Time range')
-
-flag = False
-with st.form("Choose Token & Date range"):
-    # Select token
-    st.write("Select token ticker")
-    token = st.selectbox('Select token',
+st.header('Choose Token')
+token = st.selectbox('Select token ticker',
                 options=('Select token','ETH','UNI','LINK','APE'),
-                index=0, label_visibility='collapsed')
+                index=0)
+
+st.header(f'Real-time {token} Token Metrics' if token != "Select token" else "Real-time Token Metrics")
+col1, col2, col3, col4 = st.columns(4)
+if token != "Select token":
+    coin_data = call_coin_data(token)
+    
+token_price = coin_data['prices'][0][1] if token != "Select token" else 0.00
+token_market_cap = coin_data['market_caps'][0][1] / 1000000 if token != "Select token" else 0.0
+token_volume = coin_data['total_volumes'][0][1] / 1000000 if token != "Select token" else 0.0
+col1.metric("Current price", f"${token_price:,.2f}")
+col2.metric("Market Cap", f"${token_market_cap:,.1f}M")
+col3.metric("Volume (24h)", f"${token_volume:,.1f}M")
+with col4:
+    img_url = ticker2imgurl[token] if token != 'Select token' else ticker2imgurl['BTC'] # this image is for ETH only. Need to change here for each token
+    st.image(img_url, caption='Last 7 days Price Chart', use_column_width=True)
+
+st.header('Choose Datetime range')
+with st.form("Choose Date range"):
     # Select date range
     st.write('Select date range (Time Zone: UTC)')
     min_date = dt.date(2022, 11, 19) # modify here
@@ -59,15 +103,14 @@ with st.form("Choose Token & Date range"):
         elif user_end_date < user_start_date:
             st.warning('End date must fall after start date.')
         elif user_start_date < min_date:
-            st.warning('Start date must be after 2020-01-01.')
+            st.warning(f'Start date must be after {min_date}.')
         elif user_end_date > max_date:
-            st.warning('End date must be before 2021-12-31.')
-        else:
-            flag = True
+            st.warning(f'End date must be before {max_date}.')
 
 with st.form("Choose Time range"):
     # Select time range
-    disabled = False if flag else True
+    st.write('Select time range')
+    disabled = False if date_submitted else True
     user_start_time, user_end_time = st.slider('Select time range',
         min_value=dt.datetime.combine(user_start_date, dt.time(0, 0)),
         max_value=dt.datetime.combine(user_end_date, dt.time(23, 59)),
@@ -77,40 +120,23 @@ with st.form("Choose Time range"):
         disabled=disabled)
     time_submitted = st.form_submit_button("Submit")
 
+    if time_submitted:
+        with st.spinner("Analyzing data..."):
+            time.sleep(1)
+        st.success("Done!")
 
+st.header('On-chain Network Visualization')
 
-if time_submitted:
-    with st.spinner("Analyzing data..."):
-        time.sleep(3)
-    st.success("Done!")
-    st.write(f"Token: {token}")
-    st.write(f"From: {user_start_date}")
-    st.write(f"To: {user_end_date}")
+# st.text(f"Token: {token}" if token != "Select token" else "Token: ")
+# st.text(f"From: {user_start_time}")
+# st.text(f"To: {user_end_time}")
 
-    st.header(f'Real-time {token} Token Metrics')
-    col1, col2, col3, col4 = st.columns(4)
-    coin_url = f"https://api.coingecko.com/api/v3/coins/{token_dict[token]}/market_chart?vs_currency=usd&days=0&interval=daily"
-    coin_data = requests.get(coin_url).json()
-    
-    token_price = coin_data['prices'][0][1] # if token != "Select token" else 0.00
-    token_market_cap = coin_data['market_caps'][0][1] / 1000000 # if token != "Select token" else 0.0
-    token_volume = coin_data['total_volumes'][0][1] / 1000000 # if token != "Select token" else 0.0
-    col1.metric("Current price", f"${token_price:,.2f}")
-    col2.metric("Market Cap", f"${token_market_cap:,.1f}M")
-    col3.metric("Volume (24h)", f"${token_volume:,.1f}M")
-    with col4:
-        img_url = "https://www.coingecko.com/coins/279/sparkline" # this image is for ETH only. Need to change here for each token
-        st.image(img_url, caption='Last 7 days Price Chart', use_column_width=True)
-    
+user_min = st.slider('Adjust MIN value',min_value=0,max_value=100,value=40) # need to modify
 
-    st.header('On-chain Network Visualization')
-    col1, _ = st.columns(2)
-    with col1:
-        user_min = st.slider('Adjust MIN value',min_value=0,max_value=100,value=40)
-    st.write(user_min)
+st.write(user_min)
 
-    if token != "Select token":
-        df = get_data(token, str(user_start_time), str(user_end_time), user_min, 50)
+if token != "Select token":
+    df = get_data(token, str(user_start_time), str(user_end_time), user_min, 50)
     
     print(str(user_start_time))
     print(str(user_end_time))
