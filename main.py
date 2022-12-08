682e0fbdfb16fbd80f64c6ce8fa15816fed18aa4
JH Gu <rnwjdgus96@gmail.com>
2022-12-08 14:17:12 +0900

    add state_session

--- main.py
+++ main.py
@@ -115,6 +115,12 @@ st.header('Choose Token')
 token = st.selectbox('Select token ticker',
                 options=('ETH','UNI','LINK','AXS'),
                 index=0)
+if token != st.session_state.get('token', None):
+    if 'start_time' in st.session_state:
+        del st.session_state['start_time']
+    if 'end_time' in st.session_state:
+        del st.session_state['end_time']
+    st.session_state['token'] = token
 
 st.header(f'Real-time {token} Token Metrics')
 col1, col2, col3, col4 = st.columns(4)
@@ -157,8 +163,8 @@ with st.form("Choose Date range"):
     # Select date range
     st.write('Select date range (Time Zone: UTC)')
     
-    user_start_date = st.date_input(f'Start date: from {min_date}', min_date, disabled=False)
-    user_end_date = st.date_input(f'End date: to {max_date}', min_date + dt.timedelta(days=2), disabled=False)
+    user_start_date = st.date_input(f'Start date: available from {min_date}', min_date, disabled=False)
+    user_end_date = st.date_input(f'End date: available until {max_date}', min_date + dt.timedelta(days=2), disabled=False)
     date_submitted = st.form_submit_button("Continue")
     if date_submitted:
         if user_end_date < user_start_date:
@@ -182,154 +188,164 @@ with st.form("Choose Time range"):
     time_submitted = st.form_submit_button("Submit")
 
     if time_submitted:
-        with st.spinner("Analyzing data..."):
-            time.sleep(1)
-        st.success("Done!")
+        st.session_state['start_time'] = user_start_time
+        st.session_state['end_time'] = user_end_time
+
+# if 'config' in st.session_state and st.session_state['config'] != None:
+#     st.success("Done!")
+# with st.spinner("Analyzing data..."):
+#     time.sleep(1)
 
 st.header('On-chain Network Visualization')
 
-st.text(f"On-chain Network of {token} from {user_start_time} to {user_end_time} (UTC)")
+if 'start_time' in st.session_state and st.session_state['start_time'] != None:
+    token = st.session_state['token']
+    user_start_time = st.session_state['start_time']
+    user_end_time = st.session_state['end_time']
 
-min_value = 0
-max_value = 0
-if token == 'ETH':
-    min_value = 10000
-    max_value = 30000
-elif token == 'LINK':
-    min_value = 100
-    max_value = 1000
-else:
-    min_value = 1000
-    max_value = 10000
+    st.text(f"On-chain Network of {token} from {user_start_time} to {user_end_time} (UTC)")
 
-user_min = st.slider('Adjust MIN value',min_value=min_value,max_value=max_value,value=min_value) # need to modify
-
-# if token != "Select token":
-df = get_data(token, str(user_start_time), str(user_end_time), user_min, 50)
-
-G = nx.from_pandas_edgelist(df, source='From', target='To', edge_attr='Value', create_using=nx.MultiGraph())
-edge_info=nx.get_edge_attributes(G,'Value')
-nx.set_node_attributes(G, 0, 'size')
-nx.set_node_attributes(G, 0, 'color')
-volume={}
-title={}
-for i in (G.nodes().keys()):
-    volume[i]={}
-    volume[i]['neighbor']=[]
-    volume[i]['amount']=[]
-    #|In value|-|Out Value| 담는 부분
-    volume[i]['adj']=[]
-
-    title[i]=''
-
-edge_data=[]
-for edge in G.edges():
-    e=list(edge)
-    e.append(0)
-    e=tuple(e)
-    # To set node size corresponding to weights
-    G.nodes[edge[0]]['size']+=edge_info[e]
-    G.nodes[edge[1]]['size']+=edge_info[e]
-    #각 노드의 이웃에 대한 정보
-    if(edge[1] not in volume[edge[0]]['neighbor']):
-        volume[edge[0]]['neighbor'].append(edge[1])
-        volume[edge[0]]['amount'].append(edge_info[e])
-        volume[edge[0]]['adj']=-edge_info[e]
+    min_value = 0
+    max_value = 0
+    if token == 'ETH':
+        min_value = 10000
+        max_value = 30000
+    elif token == 'LINK':
+        min_value = 100
+        max_value = 1000
     else:
-        idx=volume[edge[0]]['neighbor'].index(edge[1])
-        volume[edge[0]]['amount'][idx]+=edge_info[e]
-        volume[edge[0]]['adj']=edge_info[e]
-    if(edge[0] not in volume[edge[1]]['neighbor']):
-        volume[edge[1]]['neighbor'].append(edge[0])
-        volume[edge[1]]['amount'].append(edge_info[e])
-        volume[edge[1]]['adj']=edge_info[e]
-    else:
-        idx=volume[edge[1]]['neighbor'].index(edge[0])
-        volume[edge[1]]['amount'][idx]+=edge_info[e]
-        volume[edge[1]]['adj']+=edge_info[e]
-    edge_data.append(edge_info[e])
+        min_value = 1000
+        max_value = 10000
+
+    user_min = st.slider('Adjust MIN value',min_value=min_value,max_value=max_value,value=min_value) # need to modify
+
+    # if token != "Select token":
+
+    df = get_data(token, str(user_start_time), str(user_end_time), user_min, 50)
+
+    G = nx.from_pandas_edgelist(df, source='From', target='To', edge_attr='Value', create_using=nx.MultiGraph())
+    edge_info=nx.get_edge_attributes(G,'Value')
+    nx.set_node_attributes(G, 0, 'size')
+    nx.set_node_attributes(G, 0, 'color')
+    volume={}
+    title={}
+    for i in (G.nodes().keys()):
+        volume[i]={}
+        volume[i]['neighbor']=[]
+        volume[i]['amount']=[]
+        #|In value|-|Out Value| 담는 부분
+        volume[i]['adj']=[]
+
+        title[i]=''
+
+    edge_data=[]
+    for edge in G.edges():
+        e=list(edge)
+        e.append(0)
+        e=tuple(e)
+        # To set node size corresponding to weights
+        G.nodes[edge[0]]['size']+=edge_info[e]
+        G.nodes[edge[1]]['size']+=edge_info[e]
+        #각 노드의 이웃에 대한 정보
+        if(edge[1] not in volume[edge[0]]['neighbor']):
+            volume[edge[0]]['neighbor'].append(edge[1])
+            volume[edge[0]]['amount'].append(edge_info[e])
+            volume[edge[0]]['adj']=-edge_info[e]
+        else:
+            idx=volume[edge[0]]['neighbor'].index(edge[1])
+            volume[edge[0]]['amount'][idx]+=edge_info[e]
+            volume[edge[0]]['adj']=edge_info[e]
+        if(edge[0] not in volume[edge[1]]['neighbor']):
+            volume[edge[1]]['neighbor'].append(edge[0])
+            volume[edge[1]]['amount'].append(edge_info[e])
+            volume[edge[1]]['adj']=edge_info[e]
+        else:
+            idx=volume[edge[1]]['neighbor'].index(edge[0])
+            volume[edge[1]]['amount'][idx]+=edge_info[e]
+            volume[edge[1]]['adj']+=edge_info[e]
+        edge_data.append(edge_info[e])
 
 
-#Normlization for node size
-node_size=nx.get_node_attributes(G,'size')
+    #Normlization for node size
+    node_size=nx.get_node_attributes(G,'size')
 
-data=list(nx.get_node_attributes(G,'size').values())
-norm_size= [(float(i)-min(data))/(max(data)-min(data)) for i in data]
+    data=list(nx.get_node_attributes(G,'size').values())
+    norm_size= [(float(i)-min(data))/(max(data)-min(data)) for i in data]
 
-#Normalize for edge width
-width={}
-norm_edge= [(float(i)-min(edge_data))/(max(edge_data)-min(edge_data)) for i in edge_data]
+    #Normalize for edge width
+    width={}
+    norm_edge= [(float(i)-min(edge_data))/(max(edge_data)-min(edge_data)) for i in edge_data]
 
 
-#Normalization for node color
-color={}
-adj_data=[]
-for i in volume.keys():
-    adj_data.append(volume[i]['adj'])
-norm_adj=[(float(i)-min(adj_data))/(max(adj_data)-min(adj_data)) for i in adj_data]
+    #Normalization for node color
+    color={}
+    adj_data=[]
+    for i in volume.keys():
+        adj_data.append(volume[i]['adj'])
+    norm_adj=[(float(i)-min(adj_data))/(max(adj_data)-min(adj_data)) for i in adj_data]
 
-#Code RGB to 16hex
-def base10Tobase16(i):
-    base16 = "%02X" % int(i)
-    return base16
-def rgb2hex(r, g, b):
-    hex_color = "#" + base10Tobase16(r) + base10Tobase16(g) + base10Tobase16(b)
-    return hex_color
+    #Code RGB to 16hex
+    def base10Tobase16(i):
+        base16 = "%02X" % int(i)
+        return base16
+    def rgb2hex(r, g, b):
+        hex_color = "#" + base10Tobase16(r) + base10Tobase16(g) + base10Tobase16(b)
+        return hex_color
 
-#node size, color, width dictionary generation
-for i in range(len(list(volume.keys()))):
-    node_size[list(volume.keys())[i]]=norm_size[i]*100
-    #들어온 값이 많을수록 초록 # check here!
-    color[list(volume.keys())[i]]=rgb2hex(255-norm_adj[i]*255,norm_adj[i]*255,80)
+    #node size, color, width dictionary generation
+    for i in range(len(list(volume.keys()))):
+        node_size[list(volume.keys())[i]]=norm_size[i]*100
+        #들어온 값이 많을수록 초록 # check here!
+        color[list(volume.keys())[i]]=rgb2hex(255-norm_adj[i]*255,norm_adj[i]*255,80)
 
-for k in (list(edge_info.keys())):
-    e_idx=list(edge_info.keys()).index(k)
-    width[k]=norm_edge[e_idx]*50
+    for k in (list(edge_info.keys())):
+        e_idx=list(edge_info.keys()).index(k)
+        width[k]=norm_edge[e_idx]*50
 
-for i in (G.nodes().keys()):
-    argsort = np.argsort(volume[i]['amount'])[::-1]
-    volume[i]['amount'] = list(np.array(volume[i]['amount'])[argsort])
-    volume[i]['neighbor'] = list(np.array(volume[i]['neighbor'])[argsort])
+    for i in (G.nodes().keys()):
+        argsort = np.argsort(volume[i]['amount'])[::-1]
+        volume[i]['amount'] = list(np.array(volume[i]['amount'])[argsort])
+        volume[i]['neighbor'] = list(np.array(volume[i]['neighbor'])[argsort])
 
-for i in volume.keys():
-    tops="Tops Transaction Neighbors"
-    amount_s="Amount"
-    title[i]+="{:-<90}".format("-")+"\n"+" .   |"+'{0:=^42}'.format(tops)+"|"f"{amount_s:=^16}\n"+"{:-<90}".format("-")+"\n"
-    for j in range(len(volume[i]['neighbor'])):
-        if(j<5):
-            title[i]+=str(j+1)+" |  "+f"{volume[i]['neighbor'][j]:<50}"+'  |   '
-            title[i]+=f"{str(volume[i]['amount'][j]):^30}"+"\n"
-    
-#Setting up size attribute
-nx.set_node_attributes(G,node_size,'size')
-nx.set_node_attributes(G,title,'title')
-nx.set_edge_attributes(G,width,'weight')
-nx.set_node_attributes(G,color,'color')
-nx.set_edge_attributes(G,'gray','color')
+    for i in volume.keys():
+        tops="Tops Transaction Neighbors"
+        amount_s="Amount"
+        title[i]+="{:-<90}".format("-")+"\n"+" .   |"+'{0:=^42}'.format(tops)+"|"f"{amount_s:=^16}\n"+"{:-<90}".format("-")+"\n"
+        for j in range(len(volume[i]['neighbor'])):
+            if(j<5):
+                title[i]+=str(j+1)+" |  "+f"{volume[i]['neighbor'][j]:<50}"+'  |   '
+                title[i]+=f"{str(volume[i]['amount'][j]):^30}"+"\n"
+        
+    #Setting up size attribute
+    nx.set_node_attributes(G,node_size,'size')
+    nx.set_node_attributes(G,title,'title')
+    nx.set_edge_attributes(G,width,'weight')
+    nx.set_node_attributes(G,color,'color')
+    nx.set_edge_attributes(G,'gray','color')
 
-# Initiate PyVis network object
-coin_net = Network(height='1000px', bgcolor='#111111', font_color='white')
+    # Initiate PyVis network object
+    coin_net = Network(height='1000px', bgcolor='#111111', font_color='white')
 
-# Take Networkx graph and translate it to a PyVis graph format
-coin_net.from_nx(G)
+    # Take Networkx graph and translate it to a PyVis graph format
+    coin_net.from_nx(G)
 
-# Generate network with specific layout settings
-coin_net.repulsion(node_distance=500, central_gravity=0.5,
-                        spring_length=300, spring_strength=0.10,
-                        damping=0.95)
+    # Generate network with specific layout settings
+    coin_net.repulsion(node_distance=500, central_gravity=0.5,
+                            spring_length=300, spring_strength=0.10,
+                            damping=0.95)
 
-# Save and read graph as HTML file (on Streamlit Sharing)
-try:
-    path = '/tmp'
-    coin_net.save_graph(f'{path}/pyvis_graph.html')
-    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+    # Save and read graph as HTML file (on Streamlit Sharing)
+    try:
+        path = '/tmp'
+        coin_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
-# Save and read graph as HTML file (locally)
-except:
-    path = '/html_files'
-    coin_net.save_graph(f'{path}/pyvis_graph.html')
-    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+    # Save and read graph as HTML file (locally)
+    except:
+        path = '/html_files'
+        coin_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
-# Load HTML file in HTML component for display on Streamlit page
-components.html(HtmlFile.read(), height=700)
+    # Load HTML file in HTML component for display on Streamlit page
+    components.html(HtmlFile.read(), height=700)
 
